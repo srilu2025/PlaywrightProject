956db885b207bc665d21a05ba3579e75313656f2
sri <sreelakshmi.ravipati88@gmail.com>
2026-02-03 19:08:26 +0000

    committing changes

--- tests/Sample.spec.ts
+++ tests/Sample.spec.ts
@@ -2,9 +2,10 @@ import { test, expect } from "@playwright/test";
 
 test("getting title of the Amazon website", async ({ page }) => {
   await page.goto("https://www.amazon.com/");
+  await page.waitForLoadState("domcontentloaded");
 
   const title = await page.title();
   console.log(title);
 
-  expect(title).toBe("Amazon.com. Spend less. Smile more.");
+  await expect(title).toBe("Amazon.com. Spend less. Smile more.");
 });
